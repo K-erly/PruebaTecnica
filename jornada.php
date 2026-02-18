135975b693455347f9a891a11bf3c1479444e9a2
Kimberly Torres <98tkimberly@gmail.com>
2026-02-18 16:35:31 -0500

    Version 2.0 feat (Configuración de cronometro con javascript)

--- jornada.php
+++ jornada.php
@@ -38,12 +38,54 @@
     <h2>Jornada en curso</h2>
     <p>Código empleado: <?php echo ($codigo); ?></p>
     <p>Ingreso: <?php echo ($jornadaActiva['hora_entrada']); ?></p>
-
+    
     <h1 id="cronometro">00:00:00</h1>
 
-    <form action="logout.php" method="POST">
-        <button type="submit">Cerrar Jornada</button>
-    </form>
+    <button class="btn" id="cerrarJornada">Cerrar Jornada</button>
+    
     </div>
+
+    <script type="text/javascript" >
+        const horaEntrada = new Date("<?php echo ($jornadaActiva['hora_entrada']) ?>");
+        
+        const h = horaEntrada.getHours().toString().padStart(2, '0');
+        const m = horaEntrada.getMinutes().toString().padStart(2, '0');
+        const s = horaEntrada.getSeconds().toString().padStart(2, '0');
+        
+       // alert("hora entrada"+horaEntrada+ "separada "+ h+":"+m+":"+s);
+        
+       function actualizarCronometro(){
+        
+        const horaActual = new Date();
+        // const hora = horaActual.getHours();
+        // const minutos = horaActual.getMinutes();
+        // const segundos = horaActual.getSeconds();
+
+        const tiempoMilisegundos = (horaActual-horaEntrada);
+
+        if(tiempoMilisegundos < 0)return; //Para evitar tiempos negativos
+
+        const totalHoras = tiempoMilisegundos / (1000 * 60 * 60);
+
+        //Representación legible del tiempo
+        let tiempoHoras = Math.floor(totalHoras);
+        let tiempoMinutos = Math.floor((totalHoras % 1) * 60);
+        let tiempoSegundos = Math.round((((totalHoras % 1) * 60) % 1) * 60);
+
+        //Formato presentacion horas
+        // if(tiempoHoras<10) {tiempoHoras = "0"+tiempoHoras};
+        // if(tiempoMinutos<10) {tiempoMinutos = "0"+tiempoMinutos};
+        // if(tiempoSegundos<10) {tiempoSegundos = "0"+tiempoSegundos};
+        tiempoHoras=tiempoHoras.toString().padStart(2, "0");
+        tiempoMinutos=tiempoMinutos.toString().padStart(2, "0");
+        tiempoSegundos=tiempoSegundos.toString().padStart(2, "0");
+        
+    
+    //     console.log("hora entrada"+horaEntrada+ "separada "+ h+":"+m+":"+s+" hora actual "+horaActual +" separada "+hora+":"+minutos+":"+segundos);
+    // console.log(totalHoras+" "+tiempoHoras+":"+tiempoMinutos+":"+tiempoSegundos);
+        document.getElementById("cronometro").textContent = `${tiempoHoras}:${tiempoMinutos}:${tiempoSegundos}`;   
+    }
+       setInterval(actualizarCronometro,1000);
+    </script>  
 
 </body>
